69fc1a8bb78e0c5baddae76fd823bd41c51c4130
Gustavo Barros <gustavoo.barros07@gmail.com>
2019-10-23 12:22:41 -0300

    Description model updated

--- api/models.py
+++ api/models.py
@@ -5,7 +5,7 @@ from api.constants import KNOWLEDGE_CHOICES
 
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    name = models.CharField(max_length=80, blank=True, unique=True)
+    name = models.CharField(max_length=80, blank=True)
     username = models.CharField(max_length=30, blank=True, unique=True)
     age = models.IntegerField(null=True, blank=True)
     is_guest = models.BooleanField(default=True)
@@ -34,4 +34,4 @@ class StandDescription(models.Model):
     description = models.TextField(blank=True, null=True)
     knowledge = models.IntegerField(choices=KNOWLEDGE_CHOICES, null=True, blank=True)
     def __str__(self):
-        return self.stand.name + self.knowledge
+        return self.stand.name + self.get_knowledge_display()
