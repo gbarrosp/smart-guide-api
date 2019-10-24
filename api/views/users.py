ad216a4464b9c29f1a9e319ae9079baac8002799
Gustavo Barros <gustavoo_barros@hotmail.com>
2019-10-24 18:00:47 -0300

    Username updated when created new profile object

--- api/views/users.py
+++ api/views/users.py
@@ -41,7 +41,7 @@ class LogoutUserAPIView(APIView):
 @receiver(post_save, sender=User)
 def create_user_profile(sender, instance, created, **kwargs):
     if created:
-        Profile.objects.create(user=instance)
+        Profile.objects.create(user=instance, username=instance.username)
 
 @receiver(post_save, sender=User)
 def save_user_profile(sender, instance, **kwargs):
